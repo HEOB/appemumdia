0227d3d924d41841c8a0eb3e90f41234bc6b6aac
Henrique Eduardo de Oliveira Barros <apfheob@gmail.com>
2022-08-15 17:36:37 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'appem1dia',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
